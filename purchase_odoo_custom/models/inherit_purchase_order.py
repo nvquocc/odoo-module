d5b7168c09d758da7d09417d8814d034924aef67
Nguyen Van Quoc <nvquocc@gmail.com>
2022-10-14 14:29:09 +0700

    commit lan 2 ngay 14 thang 10

--- purchase_odoo_custom/models/inherit_purchase_order.py
+++ purchase_odoo_custom/models/inherit_purchase_order.py
@@ -5,6 +5,21 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
     department_id = fields.Many2one('hr.department', 'Phòng ban', required=True)
 
-    def show_name_of_purchase_order(self):
+    def button_confirm(self):
+        res = super(PurchaseOrder, self).button_confirm()
+        print("Da ke thua ")
+        return res
+
+    @api.onchange("order_line")
+    def onchange_product(self):
         for rec in self.order_line:
+            print(rec.product_id.name)
             print(rec.partner_id.name)
+
+    # @api.onchange("order_line")
+    # def _onchange_order_line(self):
+    #     for rec in self:
+    #         print(rec.product_id.variant_seller_ids.name.name)
+    #         print(rec.partner_id.name)
+    #         if rec.product_id.variant_seller_ids.name.name == rec.partner_id.name:
+    #
--- purchase_odoo_custom/models/inherit_purchase_order_line.py
+++ purchase_odoo_custom/models/inherit_purchase_order_line.py
@@ -5,11 +5,4 @@ class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
     vendor_suggest_id = fields.Many2one('res.partner', 'Nhà  cung cấp đề xuất')
 
-    @api.onchange("vendor_suggest_id")
-    def show_vendor_id(self):
-        for rec in self:
-            # print(rec.vendor_suggest_id.name)
-            # print(rec.partner_id.name)
-            print(rec.vendor_suggest_id.product_id.name)
-            print(rec.partner_id.name)
-# self.create_uid.partner_id.name || rec.partner_id.name
+
